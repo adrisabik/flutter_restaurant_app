6aa1f7e5af94c57e073a2c59e5439c9de9c41143
Adri Sabik <adrisabik@gmail.com>
2023-02-13 15:47:37 +0700

    update json

--- lib/restaurant.dart
+++ lib/restaurant.dart
@@ -7,6 +7,7 @@ class Restaurant {
   final String pictureId;
   final String city;
   final double rating;
+  final Menus menus;
   
   Restaurant({
     required this.id,
@@ -15,6 +16,7 @@ class Restaurant {
     required this.pictureId,
     required this.city,
     required this.rating,
+    required this.menus,
   });
 
   factory Restaurant.fromJson(Map<String, dynamic> restaurant) => Restaurant(
@@ -24,6 +26,46 @@ class Restaurant {
     pictureId: restaurant["pictureId"],
     city: restaurant["city"],
     rating: restaurant["rating"],
+    menus: Menus.fromJson(restaurant["menus"]),
+  );
+}
+
+class Menus {
+  final List<Food> foods;
+  final List<Drink> drinks;
+
+  Menus({
+    required this.foods,
+    required this.drinks,
+  });
+
+  factory Menus.fromJson(Map<String, dynamic> menus) => Menus(
+    foods: List<Food>.from(menus["foods"].map((x) => Food.fromJson(x))),
+    drinks: List<Drink>.from(menus["drinks"].map((x) => Drink.fromJson(x))),
+  );
+}
+
+class Food {
+  final String name;
+
+  Food({
+    required this.name,
+  });
+
+  factory Food.fromJson(Map<String, dynamic> food) => Food(
+    name: food["name"],
+  );
+}
+
+class Drink {
+  final String name;
+
+  Drink({
+    required this.name,
+  });
+
+  factory Drink.fromJson(Map<String, dynamic> drink) => Drink(
+    name: drink["name"],
   );
 }
 
