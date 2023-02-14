dcba22fc1f385f67103b95d681013bd67370869d
Adri Sabik <adrisabik@gmail.com>
2023-02-14 13:53:24 +0700

    update detail screen

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -12,11 +12,66 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Detail Screen'),
+        title: const Text('Detail Restaurant'),
       ),
-      body: Center(
-        child: Text(restaurant.name),
-      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0, 
+            vertical: 8.0
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(16.0),
+                child: Image.network(restaurant.pictureId),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                restaurant.name,
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.location_on,
+                    color: Colors.blue,
+                    size: 16,
+                  ),
+                  Text(
+                    restaurant.city,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24),
+              const Text(
+                'Description',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                restaurant.description,
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 12),
+            ],
+          )
+        )
+      )
     );
   }
 }
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_restaurant_app/detail_screen.dart';
 import 'package:flutter_restaurant_app/restaurant.dart';
-import 'dart:developer';
 
 class MainScreen extends StatelessWidget {
   static const routeName = '/restaurant_list';
@@ -14,32 +13,102 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Restaurant App'),
       ),
-      body: FutureBuilder<String>(
-        future: DefaultAssetBundle.of(context).loadString('assets/local_restaurant.json'),
-        builder: (context, snapshot) {
-          log('data: ${snapshot.data}');
-          final List<Restaurant> restaurants = parseRestaurants(snapshot.data);
-          return ListView.builder(
-            itemCount: restaurants.length,
-            itemBuilder: (context, index) {
-              return _buildRestaurantItem(context, restaurants[index]);
-            }
-          );
-        },
-      ),
+      body: 
+        SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+               Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 8.0
+                ),
+                child: const Text(
+                  'Recommended restaurants for you',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              FutureBuilder<String>(
+                future: DefaultAssetBundle.of(context).loadString('assets/local_restaurant.json'),
+                builder: (context, snapshot) {
+                  final List<Restaurant> restaurants = parseRestaurants(snapshot.data);
+                  return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: restaurants.length,
+                      itemBuilder: (context, index) {
+                        return _buildRestaurantItem(context, restaurants[index]);
+                      }
+                    );
+                },
+              ),
+            ],
+          ),
+        ),
     );
   }
 
   Widget _buildRestaurantItem(BuildContext context, Restaurant restaurant) {
     return ListTile(
-      contentPadding: const EdgeInsets.all(16),
-      leading: Image.network(
-        restaurant.pictureId,
-        width: 100,
-        fit: BoxFit.cover,
+      contentPadding: const EdgeInsets.symmetric(
+        horizontal: 16.0,
+        vertical: 8.0,
       ),
-      title: Text(restaurant.name),
-      subtitle: Text(restaurant.city),
+      leading: ClipRRect(
+        borderRadius: BorderRadius.circular(4),
+        child: Image.network(
+          restaurant.pictureId,
+          width: 80,
+          height: 80,
+          fit: BoxFit.cover,
+        ),
+      ),   
+      title: Text(
+        restaurant.name,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      isThreeLine: true,
+      subtitle: Column(
+        children: [
+          Row(
+            children: [
+              const Icon(
+                Icons.location_on,
+                color: Colors.blue,
+                size: 12,
+              ),
+              Text(
+                restaurant.city,
+                style: const TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 12),
+          Row(
+            children: [
+              const Icon(
+                Icons.star,
+                color: Colors.blue,
+                size: 12,
+              ),
+              Text(
+                restaurant.rating.toString(),
+                style: const TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+      
       onTap: () {
         Navigator.pushNamed(context, DetailScreen.routeName, arguments: restaurant);
       },
